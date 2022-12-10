9bd0428711139ee0e3b39e62330eb2c984639a8e
Pyng Haw Chen <pynghaw5@gmail.com>
2022-12-10 20:18:38 +0800

    First Successful run

--- app/src/main/java/com/example/utmfoodapp/rv_adapter/RvAdapterCategories.kt
+++ app/src/main/java/com/example/utmfoodapp/rv_adapter/RvAdapterCategories.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.utmfoodapp.R
 
 class RvAdapterCategories : RecyclerView.Adapter<CategoriesViewHolder>(){
 
